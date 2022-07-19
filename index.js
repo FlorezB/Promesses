c726829f35fb32b18d67375aa0252202234a55b5
Caroline <alyseflorez@hotmail.fr>
2022-07-19 14:35:23 +0200

    Hello World

---
+++ index.js
@@ -0,0 +1,14 @@
+const hello = () => {
+    return new Promise((resolve, reject) => {
+        
+        setTimeout(() => {
+            resolve("Hello World")
+        }, 2000)
+    })
+}
+
+
+
+
+
+// const temperature = 2
